3b5219f7cb7cb4ca1a006f55fbc8a99f177d8093
MER <mer.xuanyi@gmail.com>
2024-08-07 17:16:01 +0800

    Improve code readability
    Signed-off-by: MER <mer.xuanyi@gmail.com>

--- 02_echo_server_and_client_multithread/server.cc
+++ 02_echo_server_and_client_multithread/server.cc
@@ -8,24 +8,28 @@
 
 struct Socket {
   using entity_addr_t = sockaddr_in;
-  int fd;
+  int fd = -1;
   entity_addr_t addr;
+
+protected:
   Socket() = default;
-  Socket(int fd, entity_addr_t addr) : fd(fd), addr(addr) {}
 };
 
-Socket create_socket() {
-  auto fd = ::socket(AF_INET, SOCK_STREAM, 0);
-  if (fd == -1) {
-    ::perror("Socket failed");
-    ::exit(EXIT_FAILURE);
+struct ServerSocket : public Socket {
+  ServerSocket() {
+    fd = ::socket(AF_INET, SOCK_STREAM, 0);
+    if (fd == -1) {
+      ::perror("Socket failed");
+      ::exit(EXIT_FAILURE);
+    }
   }
+};
 
-  auto addr = sockaddr_in();
-  return {fd, addr};
-}
+struct ClientSocket : public Socket {
+  ClientSocket() = default;
+};
 
-void bind(Socket sock, std::string_view ip, int port) {
+void bind(ServerSocket sock, std::string_view ip, int port) {
   sock.addr.sin_family = AF_INET;
   sock.addr.sin_port = htons(port);
   ::inet_pton(AF_INET, ip.data(), &sock.addr.sin_addr);
@@ -39,7 +43,7 @@ void bind(Socket sock, std::string_view ip, int port) {
   }
 }
 
-void listen(Socket sock) {
+void listen(ServerSocket sock) {
   if (auto ret = ::listen(sock.fd, 10); ret != 0) {
     ::perror("Listen failed");
     ::exit(EXIT_FAILURE);
@@ -51,7 +55,7 @@ int main(int argc, char *argv[]) {
   auto server_ip = "127.0.0.1";
   auto server_port = 7997;
 
-  auto server_sock = create_socket();
+  auto server_sock = ServerSocket();
   bind(server_sock, server_ip, server_port);
   listen(server_sock);
 
@@ -62,30 +66,31 @@ int main(int argc, char *argv[]) {
   auto threads = std::vector<std::thread>{};
 
   while (true) {
-    auto client_addr = sockaddr_in();
-    auto client_addr_len = socklen_t(sizeof(client_addr));
 
-    auto client_fd =
-        ::accept(server_sock.fd, (sockaddr *)&client_addr, &client_addr_len);
-    if (client_fd <= 0) {
+    auto client_sock = ClientSocket();
+    auto sa_len = socklen_t(sizeof(client_sock.addr));
+
+    client_sock.fd =
+        ::accept(server_sock.fd, (sockaddr *)&client_sock.addr, &sa_len);
+    if (client_sock.fd <= 0) {
       ::perror("Bad file descriptor");
     }
 
-    auto process_client = [&](int client_fd, sockaddr_in &&client_addr) {
+    auto process_client = [&](ClientSocket &&client) {
       char client_ip[INET_ADDRSTRLEN] = {};
-      inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, INET_ADDRSTRLEN);
-      auto client_port = ntohs(client_addr.sin_port);
+      inet_ntop(AF_INET, &client.addr.sin_addr, client_ip, INET_ADDRSTRLEN);
+      auto client_port = ntohs(client.addr.sin_port);
 
       std::cout << "server[" + std::string(server_ip) + ":" +
                        std::to_string(server_port) +
                        "]: connection established with client[" +
                        std::string(client_ip) + ":" +
                        std::to_string(client_port) + "]("
-                << client_fd << ")" << std::endl;
+                << client.fd << ")" << std::endl;
 
       while (true) {
         char buffer[BUFSIZ] = {};
-        if (auto len = ::read(client_fd, buffer, BUFSIZ); len < 0) {
+        if (auto len = ::read(client.fd, buffer, BUFSIZ); len < 0) {
           ::perror("Error reading");
           ::exit(EXIT_FAILURE);
         } else if (len == 0) {
@@ -103,17 +108,17 @@ int main(int argc, char *argv[]) {
                            ":" + std::to_string(client_port) + "]: "
                     << std::string(buffer) << std::endl;
 
-          ::send(client_fd, buffer, len, 0);
+          ::send(client.fd, buffer, len, 0);
 
           // simulate processing
           std::this_thread::sleep_for(std::chrono::seconds(5));
         };
       }
 
-      ::close(client_fd);
+      ::close(client.fd);
     };
 
-    threads.emplace_back(process_client, client_fd, std::move(client_addr));
+    threads.emplace_back(process_client, std::move(client_sock));
   }
 
   // maybe join here
